90d87c93e0a59f25208b11036c40b3eb02bbd25c
andresariasm <andresariasmedina@hotmail.com>
2025-08-24 20:28:13 -0500

    main-menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginScreen from './screens/auth/LoginScreen';
 import CreateUserScreen from './screens/auth/CreateUserScreen';
-// Importaremos estas pantallas cuando las creemos
-// import MenuScreen from './screens/menu/MenuScreen';
+import ForgotPasswordScreen from './screens/auth/ForgotPasswordScreen';
+import MenuScreen from './screens/menu/MenuScreen';
 // import ProfileScreen from './screens/profile/ProfileScreen';
 // import DashboardScreen from './screens/dashboard/DashboardScreen';
-import ForgotPasswordScreen from './screens/auth/ForgotPasswordScreen';
-// import CreateUserScreen from './screens/auth/CreateUserScreen';
 
 const App: React.FC = () => {
   return (
@@ -21,12 +19,16 @@ const App: React.FC = () => {
           {/* Autenticación */}
           <Route path="/login" element={<LoginScreen />} />
           <Route path="/create-user" element={<CreateUserScreen />} />
-          {/* Pantallas principales - las crearemos después */}
-          {/* <Route path="/menu" element={<MenuScreen />} /> */}
+          <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
+          
+          {/* Pantalla principal del menú */}
+          <Route path="/menu" element={<MenuScreen />} />
+          
+          {/* Pantallas futuras */}
           {/* <Route path="/profile" element={<ProfileScreen />} /> */}
           {/* <Route path="/dashboard" element={<DashboardScreen />} /> */}
-          <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
-          {/* <Route path="/create-user" element={<CreateUserScreen />} /> */}
+          {/* <Route path="/analysis" element={<AnalysisScreen />} /> */}
+          {/* <Route path="/ai-tools" element={<AIToolsScreen />} /> */}
           
           {/* Ruta 404 - redireccionar al login */}
           <Route path="*" element={<Navigate to="/login" replace />} />
